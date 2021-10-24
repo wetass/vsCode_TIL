f80a038d8a134c421a6b3874d04c41285550687e
wetass <loark0019@naver.com>
2021-10-24 23:22:24 +0900

    자료형 조금더, 형변환
    자료형 남은것들 배움,
    형변환 : (int)등, convert, int.parse, gettype등등에 대해
    
    2021-10-24

---
+++ vsCode_2021/standard_input,output/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace standard_input_output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 입력 메서드
+            Console.WriteLine(Console.ReadLine());
+
+            Console.Write("이름을 입력하시오 : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("안녕하세요. " + name + "님");
+
+            int x = Console.Read();
+            Console.WriteLine(x);
+            Console.WriteLine(Convert.ToChar(x));
+
+            //형변환
+            //int number = 1234;
+            //long num = number;
+
+            long num1 = 1234;
+            int num2 = (int)num1;
+
+            // 변수의 크기를 벗어나는 형변환
+            long l = long.MaxValue;
+            Console.WriteLine($"l의값 : {l}");
+            int i = (int)l;
+            Console.WriteLine($"i의값 : {i}");
+
+            // Convert 형변환
+            int a = 0;
+            string b = Convert.ToString(a);
+            Int32 c = Convert.ToInt32(a);
+            double d = Convert.ToDouble(a);
+            char e = Convert.ToChar(a);
+
+            // GetType
+            int aa = 1;
+            char bb = '1';
+            string cc = "1";
+            bool dd = true;
+
+            Console.WriteLine(aa.GetType());
+            Console.WriteLine(bb.GetType());
+            Console.WriteLine(cc.GetType());
+            Console.WriteLine(dd.GetType());
+
+            Console.WriteLine();
+            // Parse 형변환
+            string strNumber = "1234";
+            int number1 = int.Parse(strNumber);
+
+            // 여러 형식으로 변환
+            Console.Write("정수를 입력하시오 : ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            Console.WriteLine($"{number} - {number.GetType()}");
+        }
+    }
+}
---
+++ vsCode_2021/string/String.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace String
+{
+    class String
+{
+    static void Main(string[] args)
+    {
+            // 문자열 보간법
+            string message = "Heelo";
+            Console.WriteLine($"문자열 : {message}");
+
+            int number = 3;
+            string result = "홀수";
+            Console.WriteLine($"{number}은 {result}이다");
+
+
+            // string.Format
+            string msg = string.Format("{0}님, {1}", "백승수", "안녕하세요");
+            Console.WriteLine(msg);
+
+            // bool 값
+            bool isTrue = true;
+            bool isFalse = false;
+
+            //const 상수
+            const int num = 10;
+            //num = 11; 불가능
+
+
+    }
+}
+}
