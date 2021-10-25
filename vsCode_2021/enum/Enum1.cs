ca6637bef0696c2b7a6c16115d420ea8bddf983e
wetass <loark0019@naver.com>
2021-10-26 00:56:02 +0900

    Learn Enum
    Enum을 조금 배움
    
    2021_10_26

---
+++ vsCode_2021/enum/Enum1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enum
+{
+    class Enum1
+    {
+        enum City
+        {
+            Seoul,
+            Deajun,
+            Busan = 5,
+            Jeju = 10
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    City myCity;
+
+        //    myCity = City.Seoul;
+
+        //    int cityValue = (int)myCity;
+
+        //    if (myCity == City.Seoul)
+        //    {
+        //        Console.WriteLine("Welcome to Seoul");
+        //    }
+        //}
+    }
+}
---
+++ vsCode_2021/enum/Enum2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Enum2
+{
+    class Enum2
+    {
+        public enum Colors
+        {
+            Red = 0,
+            Yellow,
+            Green,
+            Blue
+        }
+
+        static void Main(string[] args)
+        {
+            var color = (Colors)System.Enum.Parse(typeof(Colors), "Red");
+
+            var colorIdx = (int)Colors.Yellow;
+
+            var colorNames = System.Enum.GetNames(typeof(Colors));
+
+            Console.WriteLine(color);
+            Console.WriteLine(colorIdx);
+            Console.WriteLine(colorNames);
+        }
+    }
+}
